94315ed422e646f64a48f9c61dd62eecf690a21d
Ajitesh Kumar <ajiteshkmr802@gmail.com>
2021-06-15 00:19:40 +0530

    Update coin-change-2.cpp

--- coin-change-2.cpp
+++ coin-change-2.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     
-    int dp[5001][3001];
+    /*int dp[5001][3001];
     int CoinChangeIITopDown(int n, vector<int>& coins ,int idx , int count) {
         
         if(n==0)
@@ -15,29 +15,48 @@ public:
             return dp[n][idx];
         
        int option1 = 0 , option2 = 0; 
-       option1 = solve(n - coins[idx] , coins , idx , count);
-       option2 = solve(n , coins , idx + 1 , count);
+       option1 = CoinChangeIITopDown(n - coins[idx] , coins , idx , count);
+       option2 = CoinChangeIITopDown(n , coins , idx + 1 , count);
         
         count += (option1 + option2);
         
         return dp[n][idx] = count;    
         
-    }
+    }*/
     
     
-    int CoinChangeIIBottomUp(int amount , vector<int>& coins){
+    int CoinChangeIIBottomUp(int sum , vector<int>& coins){
         
+        int n = coins.size();   
+        int dp[n+1][sum+1];
+        
+        for(int i=0;i<n+1;i++)
+            for(int j=0;j<sum+1;j++)
+                dp[i][j] = 0;
+        dp[0][0] = 1;
+        
+        for(int i=1;i<=n;i++){ 
+            for(int j=0;j<=sum;j++){
+               
+                dp[i][j] = dp[i-1][j] ;
+                if(j - coins[i-1] >=0)
+                    dp[i][j] += dp[i][j - coins[i-1]];
+                
+            }
+        }
+        
+        return dp[n][sum];
     }
-    
-    
     
     
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
         
-        memset(dp , -1 , sizeof(dp[0])*(amount+1));
-        int count = 0;
-        return solve(amount , coins , 0 , count);
+       // memset(dp , -1 , sizeof(dp[0])*(amount+1));
+       // int count = 0;
+       // return solve(amount , coins , 0 , count);
+        
+        return CoinChangeIIBottomUp(amount , coins);
     }
 };
