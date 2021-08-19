5d7d8e901e07f701837204e3fa669db03dc6bd03
Ajitesh Kumar <ajiteshkmr802@gmail.com>
2021-08-19 11:06:22 +0530

    Update Decode Ways.cpp

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -1,43 +1,20 @@
-//Decode Ways
 class Solution {
 public:
     int numDecodings(string s) {
+        
         int n = s.size();
-        int dp[n];
-        dp[0] = 1;
-        if(s[0]=='0')
+        if(!s.size())
             return 0;
-        for(int i=1;i<n;i++){
-            if(s[i]=='0'){
-                dp[i] = 0;
-                if(i-2>=0 && (int)(s.substr(i-1,2) <=26)){
-                    dp[i] = dp[i-2];
-                }
-                if(i-2<0)
-                    dp[i] = 1;
-                
+        vector<int>dp(n+1);
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=0;
+            if(s[i]!='0'){
+                dp[i] = dp[i+1];
+                if(i<n-1 && (s[i]=='1' || (s[i]=='2' && s[i+1]<='6')))
+                    dp[i] += dp[i+2];
             }
-            else {
-                dp[i] = dp[i-1];
-                if(int(s.substr(i-1 ,2)<=26))
-                    dp[i] = dp[i-1];
-            }
-
         }
-        
-        return dp[n-1];
-        
+        return dp[0];
     }
 };
-
-/*
- 
-          2   2       6
-dp[i]     1   1 +1    3
-
-         1   1     1     0      6 
-         1   2     2+1   2      2 
-
-
-
-*/
