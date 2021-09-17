dc07c4d54421057e5ac6570ca740383ffe3ee45e
Ajitesh Kumar <ajiteshkmr802@gmail.com>
2021-09-17 23:51:52 +0530

    Create max-consecutive-ones-iii.cpp

---
+++ max-consecutive-ones-iii.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/max-consecutive-ones-iii/
+class Solution {
+public:
+    
+    
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int l = 0 , r = -1 , flips = k  , cnt = 0 , ans = 0 , n = nums.size();
+        while(r < n-1){
+            r++;
+            if(nums[r]==0){
+                flips--;
+                while(flips<0){
+                        if(nums[l]==0)
+                            flips++;
+                        l++;
+                    }
+                ans = max(ans , r - l +1); 
+            }
+            if(flips>=0)
+                ans = max(ans , r - l +1);   
+        }
+        return ans;
+    }
+};
